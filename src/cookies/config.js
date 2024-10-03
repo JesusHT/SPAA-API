6dc3f960499b994b0fae1802deb80801bdab8546
jesusht <jesus43.gts@gmail.com>
2024-10-02 22:49:34 -0600

    update auth

--- src/cookies/config.js
+++ src/cookies/config.js
@@ -4,7 +4,6 @@ const user    = require('../auth/authToken')
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-  console.log(req)
     const token = req.cookies.token || req.headers.authorization;
     if (!token) {
       return res.status(401).json({ message: 'No token provided' });
